69fc4a6c7842dc205e0c3fec580b59f97668cf67
Laurynas Butkus <laurynas.butkus@gmail.com>
2018-06-16 15:44:09 +0300

    Fix startup error when after_fork hooks are not defined
    https://github.com/vinted/sidekiq-pool/issues/26

--- lib/sidekiq/pool.rb
+++ lib/sidekiq/pool.rb
@@ -5,12 +5,11 @@ module Sidekiq
     module_function
 
     def after_fork(&block)
-      @after_fork_hooks ||= []
-      @after_fork_hooks << block
+      after_fork_hooks << block
     end
 
     def after_fork_hooks
-      @after_fork_hooks
+      @after_fork_hooks ||= []
     end
   end
 end
